1b49b49d993f3bc9a13fdef05080444cdf82c8cf
Raquel <rakiterrazas@gmail.com>
2021-12-13 20:28:16 -0400

    netlify add

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const app = require('index');
+const app = require('./express/server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(5000, () => console.log('Local app listening on port 5000!'));
